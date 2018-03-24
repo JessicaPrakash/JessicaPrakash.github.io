6df287ad55852f706965c57b9891d572bfdea71b
JessicaPrakash <35860442+JessicaPrakash@users.noreply.github.com>
2018-03-24 15:43:58 -0700

    Update sketch.js

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,17 +1,19 @@
+function setup () {
+  createCanvas (500, 400);
+  background (51);
+  translate (width / 2, height / 2);
 
-// declare variables here
+  var r = 100;
+  beginShape();
+  for (var a = -PI / 2; a < PI / 2; a += 0.1); {
+    var x = r * cos(a);
+    var y = r * sin(a);
+    stroke(255);
+    strokeWeight(4);
+    //point(x,y);
+    vertex(x, y);
+    r--;
+  }
+  endShape();
 
-
-// setup runs once
-function setup() {
-	createCanvas(500, 500);
-	background(150);
 }
-
-// draw loops and loops
-function draw() {
-
-}
-
-
-// write functions here
