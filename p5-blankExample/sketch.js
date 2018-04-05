6b4ed6e6d0765f7d0825b478d11501138488bcea
JessicaPrakash <35860442+JessicaPrakash@users.noreply.github.com>
2018-04-04 22:15:05 -0700

    Update sketch.js

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,47 +1,64 @@
-// Where is the circle
-var x, y;
+var yoff = 0;
 var x;
 var y;
 var outsideRadius = 150;
 var insideRadius = 100;
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(600, 500);
   // Starts in the middle
-  x = width / 2;
-  y = height / 1;
+   x = width / 2;
+   y = height;
+
 }
 
 function draw() {
-  background(200);
-  // Draw a circle
-  stroke(50);
-  fill(66, 75, 244);
-  ellipse(x, y, 24, 24);
+  background(119, 255, 255);
+  translate(width / 2, height / 2);
+  //rotate(PI / 2);
+
+  if (mouseIsPressed) {
+		fill(0, 23, 237);
+
+	} else {
+		fill(255, 100, 50);
+
+  stroke(255);
+  fill(195, 69, 249);
+  strokeWeight(1);
 
   // Jiggling randomly on the horizontal axis
-  x = x + random(-1, 1);
-  // Moving up at a constant speed
-  y = y - 1;
+x = x + random(-1, 1);
+// Moving up at a constant speed
+y = y - PI/2;
 
-  // Reset to the bottom
-  if (y < 0) {
-    y = height;
+// Reset to the bottom
+if (y < PI/2) {
+  y = height;
+}
+
+}
+
+var da = PI / 200;
+var dx = 0.05;
+
+  var xoff = 0;
+  beginShape();
+  for (var a = 0; a <= TWO_PI; a += da) {
+    var n = noise(xoff, yoff);
+    var r = sin(2 * a) * map(n, 0, 1, 50, 200);
+    var x = r * cos(a);
+    var y = r * sin(a);
+    if (a < PI){
+        xoff += dx;
+    } else{
+        xoff -= dx;
+    }
+    //point(x, y);
+    vertex(x, y);
+
   }
-  var numPoints = int(map(mouseX, 0, width, 6, 60));
-   var angle = 0;
-   var angleStep = 180.0/numPoints;
+  endShape();
 
-   beginShape(TRIANGLE_STRIP);
-   for (var i = 0; i <= numPoints; i++) {
-     var px = x + cos(radians(angle)) * outsideRadius;
-     var py = y + sin(radians(angle)) * outsideRadius;
-     angle += angleStep;
-     vertex(px, py);
-     px = x + cos(radians(angle)) * insideRadius;
-     py = y + sin(radians(angle)) * insideRadius;
-     vertex(px, py);
-     angle += angleStep;
-   }
-   endShape();
+  yoff += 0.01;
 }
