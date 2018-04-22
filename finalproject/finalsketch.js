fa20ced56a1362a50a42a30af0cc27ac0129e1cb
JessicaPrakash <35860442+JessicaPrakash@users.noreply.github.com>
2018-04-22 15:51:52 -0700

    Update finalsketch.js

--- finalproject/finalsketch.js
+++ finalproject/finalsketch.js
@@ -1,26 +1,23 @@
-function setup() {
-    createCanvas(500, 500);
-    background(200);
-    strokeWeight(10);
+var pg;
+
+function setup(){
+  createCanvas(1366, 546);
+  pg = createGraphics(700, 350);
 }
 
-function draw() {
-  if (mouseX > 400) {
-    fill(255, 100, 50);
-  } else if (mouseX > 300) {
-    fill(200, 100, 100);
-    } else if (mouseX > 200) {
-        fill(150, 100, 150);
-    } else if (mouseX > 100) {
-        fill(100, 100, 200);
-  } else {
-    fill(50, 100, 250);
-  }
-    // A triangle!
-  	triangle(50, 400, 150, 400, 100, 480);
-    // A quad!
-    quad(30, 100, 300, 450, 350, 150, 200, 50);
-    // A text!
-		textSize(16);
-    text("CHANGE COLOR WITH BREAKPOINTS", width/2 - 50, 475);
+function draw(){
+  fill(0, 255, 250);
+  rect(0, 0, width, height);
+  fill(203, 0, 255);
+  noStroke();
+  ellipse(mouseX, mouseY, 50, 50);
+
+  pg.background(0,0,0);
+  pg.noFill();
+  pg.stroke(255);
+  pg.ellipse(mouseX-330, mouseY-90, 50, 50);
+
+  //Draw the offscreen buffer to the screen with image()
+  image(pg, 330, 90);
+
 }
