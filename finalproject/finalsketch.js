164587687614b019ab68e61712e434d60dc6222e
JessicaPrakash <35860442+JessicaPrakash@users.noreply.github.com>
2018-04-28 23:04:27 -0700

    Update finalsketch.js

--- finalproject/finalsketch.js
+++ finalproject/finalsketch.js
@@ -1,11 +1,11 @@
 let rectangles = [];
 
-var sourceText = "I'm really sad!";
+var sourceText = "";
 
 var pg;
 
 function setup(){
-  createCanvas(1366, 546);
+  createCanvas(1349, 546);
   pg = createGraphics(700, 600);
 
  frameRate(20);
@@ -26,6 +26,78 @@ function draw(){
   //Draw the offscreen buffer to the screen with image()
   image(pg, 0, 0);
 
+
+
+  from = color(255, 0, 0, 0.2 * 255);
+    to = color(0, 0, 255, 0.2 * 255);
+    c1 = lerpColor(from, to, .33);
+    c2 = lerpColor(from, to, .66);
+    for (var i = 0; i < 15; i++) {
+      fill(from);
+      quad(random(690, 950), random(height),
+           random(690, 950), random(height),
+           random(690, 950), random(height),
+          random(690, 950), random(height));
+      fill(c1);
+      quad(random(850, 1090), random(height),
+           random(850, 1090), random(height),
+           random(850, 1090), random(height),
+           random(850, 1090), random(height));
+      fill(c2);
+      quad(random(1005, 1265), random(height),
+           random(1005, 1265), random(height),
+           random(1005, 1265), random(height),
+           random(1005, 1265), random(height));
+      fill(to);
+      quad(random(1185, 1415), random(height),
+           random(1185, 1415), random(height),
+           random(1185, 1415), random(height),
+           random(1185, 1415), random(height));
+
+
+               fill(from);
+               quad(random(-40, 220), random(height),
+                    random(-40, 220), random(height),
+                    random(-40, 220), random(height),
+                   random(-40, 220), random(height));
+               fill(c1);
+               quad(random(140, 380), random(height),
+                    random(140, 380), random(height),
+                    random(140, 380), random(height),
+                    random(140, 380), random(height));
+               fill(c2);
+               quad(random(320, 580), random(height),
+                    random(320, 580), random(height),
+                    random(320, 580), random(height),
+                    random(320, 580), random(height));
+               fill(to);
+               quad(random(500, 760), random(height),
+                    random(500, 760), random(height),
+                    random(500, 760), random(height),
+                    random(500, 760), random(height));
+
+
+    }
+    frameRate(3);
+
+
+
+
+  strokeWeight(2);
+  fill(255);
+  textFont("Comic Sans MS");
+  textSize(40 + (mouseX / width)*3);
+  text("What I Think", 470, 40);
+
+
+  strokeWeight(2);
+  fill(193, 48, 255);
+  textFont("Comic Sans MS");
+  textSize(40 + (mouseX / width)*3);
+  text("The Truth", 1130, 40);
+
+
+
   fill(255);
    textSize(25);
    textAlign(RIGHT, RIGHT);
@@ -36,20 +108,16 @@ function draw(){
      sourceText.substring(left, right+1),
      width/2, height/2);
 
-     fill(255);
-       textSize(25);
-       text("I shouldn't just give up.",
-         mouseX, mouseY, 200, 200);
        if (mouseIsPressed) {
          noFill();
          stroke(0, 255, 0);
-         rect(mouseX, mouseY, 200, 100);
 
-         strokeWeight(2);
+
+    strokeWeight(2);
     fill(255);
     textFont("Comic Sans MS");
     textSize(20 + (mouseX / width)*4);
-    text("Why do so many people hurt me? I didn't do anything wrong.", 680, 200);
+    text("Why do so many people hurt me? I didn't do anything wrong.", 680, 190);
 
     strokeWeight(2);
 fill(255);
@@ -69,43 +137,41 @@ strokeWeight(2);
 fill(255);
 textFont("Comic Sans MS");
 textSize(20 + (mouseX / width)*3);
-text("I'm so stupid.", 610, 40);
+text("I'm sad and so tired of this!", 600, 310);
 
 
 strokeWeight(2);
 fill(255);
 textFont("Comic Sans MS");
 textSize(20 + (mouseX / width)*3);
-text("So many people deceived me.", 600, 310);
+text("I wish my I could escape my problems but I can't.", 600, 350);
+
+strokeWeight(2);
+fill(255);
+textFont("Comic Sans MS");
+textSize(20 + (mouseX / width)*3);
+text("I'm so stupid.", 610, 390);
+
+strokeWeight(2);
+fill(255);
+textFont("Comic Sans MS");
+textSize(20 + (mouseX / width)*3);
+text("I wish I was never born.", 630, 440);
 
 
 strokeWeight(2);
 fill(255);
 textFont("Comic Sans MS");
 textSize(20 + (mouseX / width)*3);
-text("School is hard and too stressful.", 600, 350);
+text("I don't know if I can do this but I can't just give up.", 650, 500);
+
+
 
 strokeWeight(2);
-fill(255);
-textFont("Comic Sans MS");
-textSize(20 + (mouseX / width)*3);
-text("I don't know if I can do this but I can't just give up.", 610, 390);
-
-strokeWeight(2);
-fill(255);
-textFont("Comic Sans MS");
-textSize(20 + (mouseX / width)*3);
-text("I have to try letting go of the things that bring me down.", 630, 440);
-
-
-
-
-
-    strokeWeight(2);
 fill(255);
 textFont("Comic Sans MS");
 textSize(20 + (mouseX / width)*4);
-text("I'm blessed.", 1100, 200);
+text("I'm blessed", 1100, 190);
 
 strokeWeight(2);
 fill(255);
@@ -120,12 +186,6 @@ textFont("Comic Sans MS");
 textSize(20 + (mouseX / width)*3);
 text("I want to try staying positive.", 1200, 85);
 
-
-strokeWeight(2);
-fill(255);
-textFont("Comic Sans MS");
-textSize(20 + (mouseX / width)*3);
-text("Overthinking can cause us to believe lies.", 1210, 40);
 
 
 strokeWeight(2);
@@ -145,7 +205,7 @@ strokeWeight(2);
 fill(255);
 textFont("Comic Sans MS");
 textSize(20 + (mouseX / width)*3);
-text("I don't know what's going to happen in the future.", 1280, 390);
+text("No matter how hard life gets, we can't lose hope.", 1280, 390);
 
 strokeWeight(2);
 fill(255);
@@ -154,8 +214,26 @@ textSize(20 + (mouseX / width)*3);
 text("I have to remember that failure is a part of success.", 1300, 440);
 
 
+strokeWeight(2);
+fill(255);
+textFont("Comic Sans MS");
+textSize(20 + (mouseX / width)*3);
+text("I have to try letting go of the things that bring me down.", 1310, 500);
+
+
+
+strokeWeight(2);
+  fill(16, 0, 255 + sin(frameCount*0.1) * 128);
+  if (mouseIsPressed) {
+    stroke(255);
+  }
+  else {
+    noStroke();
+  }
+  textSize(20 + (mouseX / width)*72);
+  text("Don't give up. You got this!", 1150, 265);
+
 
        }
-
 
 }
